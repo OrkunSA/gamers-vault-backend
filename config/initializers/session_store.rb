fb22b86793c8587bc13bd08049b3fe6d9d571129
OrkunSA <c.orkun.s@gmail.com>
2021-01-26 23:15:47 -0600

    create session store

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,6 @@
+Rails.application.config.session_store :cookie_store, :key => '_gamers_vault'
+# if Rails.env.production == "production"
+    # Rails.application.config.session_store :cookie_store, :key => '_gamers_vault', domain:
+# else 
+# end
+
