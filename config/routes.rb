27c61e6cd0e0d93c6c7ba23a7870cf5763fe69ff
OrkunSA <c.orkun.s@gmail.com>
2021-02-01 18:58:35 -0600

    add collection and games/:id route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   resources :sessions, only: [:create]
-  resources :users, only: [:create]
+  resources :games
+
+  #USER ROUTES
+  get 'user/collections', to: 'users#collection'
+  resources :users, only: [:create, :index]
+
+  #SESSION ROUTES
   get 'logged_in', to: 'sessions#logged_in'
   delete 'logout', to: 'sessions#logout'
+
+  #GAME ROUTES
+  post '/games/:id', to: 'games#game_collection'
+
 end
